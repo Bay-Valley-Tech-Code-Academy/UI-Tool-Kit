20e2ac160b7b2b913bb7db74e2850e9d69d7f457
NathanDuncan-coder <nathanduncan485@gmail.com>
2024-08-12 14:36:32 -0700

    About page done

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -27,14 +27,68 @@ export default function Example() {
           className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
         />
       </div>
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className=" mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Bay Valley Tech</h2>
           <p className="mt-6 text-lg leading-8 text-gray-300">
           Bay Valley Tech is a fast-growing tech incubator, tech community and digital workforce training program.
           </p>
         </div>
+
+        <p className="mt-6 text-lg leading-8 text-gray-300 gap-5">
+               Our mission is to empower hardworking individuals by enabling them to attain lucrative technology-based careers, 
+               which can be transformative for their families and communities
+        </p>
+
+            <div className ="justify-evenly" >
+                    <h3 className=" flex flex-col py-5 text-4xl font-bold tracking-tight text-white sm:text-6xl ">
+                      What We Do?
+
+                        <p className=" flex flex-col py-5  mt-6 text-lg leading-8 text-gray-300 gap-5">
+                             Bay Valley Tech offers free and low-cost coding programs for students focused on web-based development languages ranging from HTML / CSS to javascript and more.
+                        </p>
+                    </h3>           
+            </div>
+
+                <div>
+                    <h4 h3 className=" flex flex-col py-5  text-4xl font-bold tracking-tight text-white sm:text-6xl ">
+                        Here are some programs we offers
+
+                        <p className=" flex flex-col py-5 mt-6 text-lg leading-8 text-gray-300 ">
+                           Free Code Academy: Bay Valley Tech provides free coding bootcamps where students learn how to code and gain valuable skills. The program emphasizes collaborative learning, seasoned instructors, and immersive internships within authentic tech environments. 
+                           It’s a great place to launch a tech career
+                        </p>
+
+                        <p className=" flex flex-col py-5 mt-6 text-lg leading-8 text-gray-300 ">
+                        Digital Skills Academy: Focused on essential digital skills.
+                        </p>
+
+                        <p className=" flex flex-col py-5 mt-6 text-lg leading-8 text-gray-300 ">
+                        Networking Opportunities: Alumni connect with successful industry professionals, creating valuable networking relationships for ongoing support and growth
+                        </p>
+
+                    </h4>
+
+                    <h4 h3 className=" flex flex-col py-5  text-4xl font-bold tracking-tight text-white sm:text-6xl ">
+                        Our Internships
+                    </h4>
+
+                        <p className=" flex flex-col py-5 mt-6 text-lg leading-8 text-gray-300 ">
+                        Get hands-on experience and jumpstart your career in tech. Enroll in our Code Academy or Digital Skills Bootcamp to qualify.
+                        </p>
+
+                        <p className=" flex flex-col py-5 mt-6  text-lg leading-8 text-gray-300 ">
+                       Our  Current Bay Valley Tech Internships:  Tech Sales Intern (Merced County Only), Partnership Management Intern (Merced County Only), Software Development
+, Digital Marketing/Digital Design (graphic design, photography, video, web design, UX/UI)
+, Technology Instructor
+                        </p>
+
+                </div>
+
       </div>
+
+              
+
     </div>
   )
 }
