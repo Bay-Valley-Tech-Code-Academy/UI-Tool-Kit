dd523b4aa1a2ef4451e56bc89997190e399d73a4
Soraya <60835721+sorayaboza@users.noreply.github.com>
2024-08-13 16:22:45 -0700

    replaced the product list with a postgres database

--- src/app/productlists/page.js
+++ src/app/productlists/page.js
@@ -1,7 +1,20 @@
-import products from '../data/products';
+import pool from "../data/postgres"; // Adjust the import path according to your structure
 
+export default async function ProductList() {
+  let products = [];
+  
+  try {
+    const client = await pool.connect();
+    console.log("Connected to the database!");
 
-export default function ProductList() {
+    const result = await client.query("SELECT * FROM productlist");
+    products = result.rows;
+
+    client.release(); // Release the client back to the pool
+  } catch (error) {
+    console.log("Error fetching data: ", error);
+  }
+
   return (
     <div className="bg-gradient-to-r from-[#3D3860] via-[#392F5A] to-[#3F3D64]">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
@@ -10,22 +23,22 @@ export default function ProductList() {
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-              <a
+            <a
               key={product.id}
               href={`/productlists/${product.id}`}
               className="group relative bg-gradient-to-b from-[#FFF8F0] to-[#FFF8F0] shadow-lg rounded-[11px] block opacity-90"
               style={{
-              boxShadow: '3px 8px 15.5px 3px rgba(34, 0, 85, 0.3)',
-              textDecoration: 'none',
+                boxShadow: '3px 8px 15.5px 3px rgba(34, 0, 85, 0.3)',
+                textDecoration: 'none',
               }}
-              >
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-[11px] bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                  <img
-                    alt={product.imageAlt}
-                    src={product.imageSrc}
-                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                  />
-                </div>
+            >
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-[11px] bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img
+                  alt={product.imagealt}
+                  src={product.imagesrc}
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
               <div className="mt-4 p-4 text-center">
                 <h3 className="text-lg font-bold text-[#392F5A]">
                   {product.name}
@@ -42,5 +55,5 @@ export default function ProductList() {
         </div>
       </div>
     </div>
-  )
+  );
 }
