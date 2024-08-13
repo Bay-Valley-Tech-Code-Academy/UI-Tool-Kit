fc08a19d55b100eab6dfb0fa4002f8510c4d47ce
Misael Bolainez <misaelbolainez@gmail.com>
2024-08-13 16:25:59 -0700

    fix(image error): Fixed the error with the image

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
-import bvtlogo from '../images/bvt-logo.png'
 
 export default function Navbar() {
 
     return (
         <nav className="md:flex sticky bg-indigo-900 opacity-100 p-4 px-20 gap-50">
             <div className="flex justify-start">
-                <Image src={bvtlogo} className="w-14 rounded-full" />
+                <Image src='/images/bvt-logo.png' className="w-14 rounded-full" />
             </div>
             <div className="flex flex-1 justify-center gap-7 mt-1 text-white font-medium text-xl">
                 <button className="hover:bg-gradient-to-r from-indigo-500 to-violet-700 p-3 rounded-lg drop-shadow-xl">Home</button>
