7f875f3fae6d5e40c53fcf78f2c2046e9cd1d490
Misael Bolainez <misaelbolainez@gmail.com>
2024-08-14 13:49:51 -0700

    fix(logo): Logo is now in the navbar

--- src/app/Footer.jsx
+++ src/app/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
           <div className="footer-column">
             <div className="footer-logo">
               <Image
-                src="/images/bvt_logo.png"
+                src="/images/bvt-logo.png"
                 alt="Logo"
                 width={70}
                 height={70}
