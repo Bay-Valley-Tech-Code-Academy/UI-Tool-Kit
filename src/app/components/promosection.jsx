0f869757aa057c4973ed229f4e154e5220aadd75
kimbencinas <kimbencinas@gmail.com>
2024-08-16 15:01:42 -0700

    promotion section overall design complete

--- src/app/components/promosection.jsx
+++ src/app/components/promosection.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 export default function PromoSection() {
 
     return (
-        <div className="bg-[#D9D9D9] justify-center text-center mt-4 py-4">
-            <h3 className="text-xl font-medium">Promotional Products</h3>
-            <p>Save up to 35% with code BVT2024</p>
-            <p className="text-end">See details</p>
+        <div className="bg-[#D9D9D9] justify-center text-center -mb-8 py-4">
+            <h3 className="sm:text-md text-xl font-semibold">Promotional Products</h3>
+            <p>Save up to 35% with code{' '}<span className="font-medium underline">BVT2024</span></p>
+            <p className="sm:text-center text-end mr-4 underline">See details</p>
         </div>
     )
 }
