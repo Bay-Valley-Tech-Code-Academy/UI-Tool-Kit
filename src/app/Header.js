774d0b45ae636b06b01ac67761760ba6ccfbb8eb
Javier <javier.miranda4420@gmail.com>
2024-08-20 14:12:19 -0700

    changed the text color on the navbar buttons to white

--- src/app/Header.js
+++ src/app/Header.js
@@ -11,18 +11,18 @@ export default function Example() {
     <header className="bg-[#3D3860]">
       <nav
         aria-label="Global"
-        className=" flex w-full items-center justify-between p-6 lg:px-8"
+        className="flex w-full items-center justify-between p-6 lg:px-8"
       >
         <img
           alt="Bay Valley Tech Light Bulb."
           src="/images/bvt-logo.png"
-          className=" h-10 w-10 object-cover object-center lg:h-20 lg:w-20"
-        ></img>
+          className="h-10 w-10 object-cover object-center lg:h-20 lg:w-20"
+        />
         <div className="flex lg:hidden">
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
           >
             <span className="sr-only">Open main menu</span>
             <Bars3Icon aria-hidden="true" className="h-6 w-6" />
@@ -30,24 +30,24 @@ export default function Example() {
         </div>
 
         <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-          <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
+          <a href="/" className="text-sm font-semibold leading-6 text-white">
             Home
           </a>
           <a
             href="/productlists"
-            className="text-sm font-semibold leading-6 text-gray-900"
+            className="text-sm font-semibold leading-6 text-white"
           >
             Product Lists
           </a>
           <a
             href="/cart"
-            className="text-sm font-semibold leading-6 text-gray-900"
+            className="text-sm font-semibold leading-6 text-white"
           >
             Shopping Cart
           </a>
           <a
             href="/about"
-            className="text-sm font-semibold leading-6 text-gray-900"
+            className="text-sm font-semibold leading-6 text-white"
           >
             About
           </a>
@@ -75,25 +75,25 @@ export default function Example() {
               <div className="space-y-2 py-6">
                 <a
                   href="/"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
                 >
                   Home
                 </a>
                 <a
                   href="/productlists"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
                 >
                   Product Lists
                 </a>
                 <a
                   href="/cart"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
                 >
                   Shopping Cart
                 </a>
                 <a
                   href="/about"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
                 >
                   About
                 </a>
--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -8,19 +8,19 @@ export default function Navbar() {
         <Image src="/images/bvt-logo.png" className="w-14 rounded-full" />
       </div>
       <div className="flex flex-1 justify-center gap-7 mt-1 text-white font-medium text-xl">
-        <button className="hover:bg-gradient-to-r from-indigo-500 to-violet-700 p-3 rounded-lg drop-shadow-xl">
+        <button className="hover:bg-gradient-to-r from-indigo-500 to-violet-700 p-3 rounded-lg drop-shadow-xl text-white">
           Home
         </button>
-        <button className="hover:bg-gradient-to-r from-indigo-500 to-violet-700 p-3 rounded-lg drop-shadow-xl">
+        <button className="hover:bg-gradient-to-r from-indigo-500 to-violet-700 p-3 rounded-lg drop-shadow-xl text-white">
           Product List
         </button>
-        <button className="hover:bg-gradient-to-r from-indigo-500 to-purple-600 p-3 rounded-lg drop-shadow-xl">
+        <button className="hover:bg-gradient-to-r from-indigo-500 to-purple-600 p-3 rounded-lg drop-shadow-xl text-white">
           Cart
         </button>
-        <button className="hover:bg-gradient-to-r from-indigo-500 to-violet-700 p-3 rounded-lg drop-shadow-xl">
+        <button className="hover:bg-gradient-to-r from-indigo-500 to-violet-700 p-3 rounded-lg drop-shadow-xl text-white">
           About
         </button>
-        <button className="hover:bg-gradient-to-r from-indigo-500 to-violet-700 p-3 rounded-lg drop-shadow-xl">
+        <button className="hover:bg-gradient-to-r from-indigo-500 to-violet-700 p-3 rounded-lg drop-shadow-xl text-white">
           Contact Us
         </button>
       </div>
