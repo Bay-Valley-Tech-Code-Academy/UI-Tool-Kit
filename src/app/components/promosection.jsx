8cdf57e3d60768842a346d9bd4122c79732fd951
Kimberly Encinas <kimbencinas@gmail.com>
2024-08-21 11:17:25 -0700

    add shadow to carousel

--- src/app/components/promosection.jsx
+++ src/app/components/promosection.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import Image from 'next/image';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../globals.css';
 
 export default function PromoSection() {
 
