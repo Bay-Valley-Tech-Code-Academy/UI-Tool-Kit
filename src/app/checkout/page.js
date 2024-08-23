952e85f81fb181927855fd771e6e68a50168e9ae
Kimberly Encinas <kimbencinas@gmail.com>
2024-08-23 11:08:14 -0700

    add checkout page skeleton

---
+++ src/app/checkout/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Checkout() {
+
+    return (
+        <div>
+            <p>Billing Details</p>
+            <p>Cart Summary</p>
+            <p>Payment method</p>
+            <button>Place order</button>
+        </div>
+    )
+}
